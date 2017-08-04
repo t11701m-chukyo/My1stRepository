bfcbae3d955dc975773955b7860cda60ed1e6a18
t11701m-chukyo <t11701m@m.chukyo-u.ac.jp>
2017-08-04 18:09:40 +0900

    Adding some files

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shimizulab/my_robot/src
-# Build directory: /home/shimizulab/my_robot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(my_opencv)
-subdirs(usb_cam)
--- src/my_opencv/src/image_converter.cpp
+++ src/my_opencv/src/image_converter.cpp
@@ -4,8 +4,13 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/superres/optical_flow.hpp> 
+
 
 static const std::string OPENCV_WINDOW = "Image window";
+
+cv::Mat capture, current, previous,flow,visual_flow, opening;
 
 class ImageConverter
 {
@@ -18,8 +23,8 @@ public:
   ImageConverter()
     : it_(nh_)
   {
-    // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("usb_cam/image_raw", 1,
+
+    image_sub_ = it_.subscribe("/usb_cam/image_raw", 1,
       &ImageConverter::imageCb, this);
     image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
@@ -43,33 +48,149 @@ public:
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
+    
+    //カメラ画像
+    capture = cv_ptr->image;
+    
+     //グレイスケールへ変換
+    cvtColor(capture, current, CV_BGR2GRAY);
+   
+    //前のフレームがあれば、オプティカルフローを計算し、表示する
+    if(!previous.empty()) {
+        //オプティカルフロー計算
+      cv::calcOpticalFlowFarneback(previous, current, flow, 0.5, 1, 4, 1, 5, 1.1,3);
+            
+        //オプティカルフローを可視化
+      opening = current.clone();
+      
+      int flow_ch = flow.channels();
+      for(int y = 0; y < opening.rows; ++y){
+      
+          float* psrc = (float*)(flow.data + flow.step * y);        
+        
+				for(int x = 0; x < opening.cols; ++x){
+				
+				  float dx = psrc[0];
+          float dy = psrc[1];
+				  float r = (dx + dy);
+						
+					if( 4 < r ){ 
+					  opening.data[ y * opening.step + x  ] = 0;
+					}else{
+					  opening.data[ y * opening.step + x  ] = 255;
+					}
+					psrc += flow_ch;
+				}
+			}
+			
+			//クロージング
+			cv::morphologyEx(
+         opening,
+         opening,
+         cv::MORPH_CLOSE,        // モルフォロジー演算の種類
+         cv::Mat(),
+         cv::Point( -1, -1 ),
+         3,                     // 収縮と膨張が適用される回数．
+         cv::BORDER_CONSTANT,
+         cv::morphologyDefaultBorderValue()
+         );
+			//オープニング
+      cv::morphologyEx(
+         opening,
+         opening,
+         cv::MORPH_OPEN,        // モルフォロジー演算の種類
+         cv::Mat(),
+         cv::Point( -1, -1 ),
+         50,                     // 収縮と膨張が適用される回数．
+         cv::BORDER_CONSTANT,
+         cv::morphologyDefaultBorderValue()
+         );
+         
+      //動く物体を囲むための座標
+      int x,y;
+      int s_x,s_y,g_x,g_y;
+      bool bEnd = false;
+      //上
+      for(y = 0; y < opening.rows; ++y){      
+				for(x = 0; x < opening.cols; ++x){
+				   if( opening.data[ y * opening.step + x ] == 0 ){
+				     s_y = y;
+				     bEnd = true;
+				   }
+				   if( bEnd ){ break; }
+				}
+				if( bEnd ){ break; }
+			}
+			bEnd = false;    
+			//右
+			for(x = 0; x < opening.cols; ++x){
+			  for(y = 0; y < opening.rows; ++y){ 
+				   if( opening.data[ y * opening.step + x ] == 0 ){
+				     s_x = x;
+				     bEnd = true;
+				   }
+				   if( bEnd ){ break; }
+				}
+				if( bEnd ){ break; }
+			}
+			bEnd = false; 
+			//下
+			for( y = opening.rows-1; -1 < y; --y){      
+				for( x = opening.cols-1; -1 < x; --x){
+				   if( opening.data[ y * opening.step + x ] == 0 ){
+				     g_y = y;
+				     bEnd = true;
+				   }
+				   if( bEnd ){ break; }
+				}
+				if( bEnd ){ break; }
+			}   
+			bEnd = false; 
+			//左
+			for( x = opening.cols-1; -1 < x; --x){
+				for( y = opening.rows-1; -1 < y; --y){ 
+				   if( opening.data[ y * opening.step + x ] == 0 ){
+				     g_x = x;
+				     bEnd = true;
+				   }
+				   if( bEnd ){ break; }
+				}
+				if( bEnd ){ break; }
+			}
+      bEnd = false; 
+      
+      
+      //表示画面
+      visual_flow = capture.clone();
+      
+      //動く物体を四角で囲む
+			cv::rectangle(visual_flow, cv::Point(s_x,s_y), cv::Point(g_x, g_y), cv::Scalar(0,0,255), 3, 4);
 
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-
-		//cv::Mat gray;
-		cv_bridge::CvImage	gray;
-		cv::cvtColor(cv_ptr->image, gray.image, CV_BGR2GRAY);
-		//cv::threshold(gray.image, gray.image, 127,255, cv::THRESH_BINARY);
-		//image_pub_.publish(
-		//		cv_bridge::CvImage(std_msgs::Header(),"bgr8",gray).toImageMsg());
-
-    // Update GUI Window
-//    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-		cv::imshow(OPENCV_WINDOW, gray.image);
+      //表示
+      cv::imshow(OPENCV_WINDOW, visual_flow);
+      //printf("OK¥n");
+      
+    }
+    //前のフレームを保存
+    previous = current.clone();
+    
+    
     cv::waitKey(3);
 
     // Output modified video stream
-//    image_pub_.publish(cv_ptr->toImageMsg());
-		image_pub_.publish(cv_bridge::CvImage(std_msgs::Header(),"mono8",gray.image).toImageMsg());
-  }
+    image_pub_.publish(cv_ptr->toImageMsg()); 
+
+
+    }
 };
+
 
 int main(int argc, char** argv)
 {
+  
   ros::init(argc, argv, "image_converter");
   ImageConverter ic;
   ros::spin();
   return 0;
+  
 }
